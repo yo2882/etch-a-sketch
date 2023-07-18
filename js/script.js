91339ae9cf6eb67a10cb720e3a3954f28e88106a
yo2882 <133942827+yo2882@users.noreply.github.com>
2023-07-18 15:50:01 +0700

    Add grid value slider / generate grid function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 //Grid generation
-for (let i = 0; i < 16; i++) {
-    const squareGrid = document.querySelector(".square-grid");
-    const squareSubGrid = document.createElement("div");
-    squareSubGrid.classList.add("row-flex");
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        squareSubGrid.appendChild(square);
+let gridSize = 16;
+const squareGrid = document.querySelector(".square-grid");
+
+function removeAllChild (parent){
+    while (parent.firstChild) {
+        parent.remove(parent.firstChild);
     }
-    squareGrid.appendChild(squareSubGrid);
 }
+
+function generateGrid() {
+    removeAllChild (squareGrid);
+    for (let i = 0; i < gridSize; i++) {
+        const squareSubGrid = document.createElement("div");
+        squareSubGrid.classList.add("row-flex");
+        for (let j = 0; j < gridSize; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            squareSubGrid.appendChild(square);
+        }
+        squareGrid.appendChild(squareSubGrid);
+    }
+}
+
+generateGrid();
 
 //track status of mousedown and mouseup in penStatus
 const body = document.querySelector("body");
@@ -17,16 +30,14 @@ let penStatus = false;
 
 function enablePen(){
     penStatus = true;
-    event.preventDefault();
 }
 
 function disablePen(){
     penStatus = false;
-    event.preventDefault();
 }
 
-body.addEventListener("mousedown",enablePen);
-body.addEventListener("mouseup",disablePen);
+document.addEventListener("mousedown",enablePen);
+document.addEventListener("mouseup",disablePen);
 
 //Change color of square when holding down the mouse and mouseover square
 function changeColor(){
@@ -50,3 +61,11 @@ function reset(){
 }
 
 resetButton.addEventListener("click",reset)
+
+//Slider to adjust grid size
+const slider = document.querySelector(".grid-slider input");
+const sliderValueDisplay = document.querySelector(".grid-slider p");
+
+slider.addEventListener("input",() => {
+    sliderValueDisplay.textContent = slider.value;
+})
