c6302d3c1dc625b1b9e9ab4207ba4e9734f5e67e
yo2882 <133942827+yo2882@users.noreply.github.com>
2023-07-19 14:31:01 +0700

    Add feature to change color mode (random,gradient)

--- js/script.js
+++ js/script.js
@@ -40,10 +40,57 @@ function disablePen(){
 document.addEventListener("mousedown",enablePen);
 document.addEventListener("mouseup",disablePen);
 
+//change color mode
+let color = "gray"
+
+const colorButtons = document.querySelectorAll(".color-button")
+
+function changeMode(event){
+    switch (event.target.value) {
+        case "gray":
+            color = "gray"//"rgb(185, 185, 185)";
+            break;
+        case "grayscale":
+            color = "grayscale"//"rgb(0, 0, 0)";
+            break;        
+        case "random":
+            color = "random"//`#${Math.floor(Math.random()*16777215).toString(16)}`;
+            break;            
+    }
+}
+
+colorButtons.forEach(colorButton => {
+    colorButton.addEventListener("click",changeMode)
+});
+
+//Change behavior of changeColor based on mode (run function everytime mouse event occur to randomize color)
+let gradientValue = 0;
+
+function changeBgColor(event) {
+    switch (color) {
+        case "gray":
+            event.target.style.backgroundColor = "rgb(185, 185, 185)";
+            break;
+        case "grayscale":
+            if (gradientValue >= 200){
+                gradientValue = 0
+            }
+            else{
+                gradientValue += 20
+            }
+            event.target.style.backgroundColor = "#"+(gradientValue).toString(16)+(gradientValue).toString(16)+(gradientValue).toString(16)
+            break;        
+        case "random":
+            event.target.style.backgroundColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+            break;
+    }
+}
+
 //Change color of square when holding down the mouse and mouseover square
-function changeColor(){
+
+function changeColor(event){
     if(penStatus){
-        this.classList.add("colored");
+        changeBgColor(event)
     }
 }
 
@@ -69,6 +116,6 @@ const slider = document.querySelector(".grid-slider input");
 const sliderValueDisplay = document.querySelector(".grid-slider p");
 
 slider.addEventListener("input",() => {
-    sliderValueDisplay.textContent = `${slider.value} x ${slider.value}`;
+    sliderValueDisplay.textContent = slider.value;
     gridSize = slider.value;
 })
